1e48a3e2a8e63b5147103cf3dfd1bde27a609ca9
Shaun Anderson <shaun.anderson@outlook.com>
2024-03-19 21:39:26 +1100

    feat: solr screenshot

--- src/components/Things.tsx
+++ src/components/Things.tsx
@@ -1,35 +1,29 @@
 import "../styles/things.css";
 import inkt from "../assets/inkt-wip.webp"; // Image is 1600x900
+import solr from "../assets/solr-wip.webp"; // Image is 1600x900
 
 type ThingCardProps = {
-  type: "project" | "art";
   link: string;
   title: string;
   description: string;
   badges?: string[];
-  img?: {src: string; alt: string};
+  img?: {src: string; alt: string, pos?: 'left' | 'bottom'};
   wip?: boolean;
+  compact?: boolean;
 };
 const ThingCard = (props: ThingCardProps) => {
-  const { type, link, title, description, badges, img, wip } = props;
+  const { link, title, description, badges, img, wip, compact } = props;
   return (
     <a className={`thing-card ${wip ? "wip" : ""}`} href={link}>
-      <article className="container">
-        <div className="details-container">
+      <article className={`container ${img?.pos === 'bottom' && 'img-below'}`}>
+        <div className={`details-container ${compact && 'compact'}`}>
           {wip && <div className="wip-badge">work in progress</div>}
-        {type === "project" && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            viewBox="0 0 256 256"
-          >
-            <path d="M176,232a8,8,0,0,1-8,8H88a8,8,0,0,1,0-16h80A8,8,0,0,1,176,232Zm40-128a87.55,87.55,0,0,1-33.64,69.21A16.24,16.24,0,0,0,176,186v6a16,16,0,0,1-16,16H96a16,16,0,0,1-16-16v-6a16,16,0,0,0-6.23-12.66A87.59,87.59,0,0,1,40,104.49C39.74,56.83,78.26,17.14,125.88,16A88,88,0,0,1,216,104Zm-16,0a72,72,0,0,0-73.74-72c-39,.92-70.47,33.39-70.26,72.39a71.65,71.65,0,0,0,27.64,56.3A32,32,0,0,1,96,186v6h64v-6a32.15,32.15,0,0,1,12.47-25.35A71.65,71.65,0,0,0,200,104Zm-16.11-9.34a57.6,57.6,0,0,0-46.56-46.55,8,8,0,0,0-2.66,15.78c16.57,2.79,30.63,16.85,33.44,33.45A8,8,0,0,0,176,104a9,9,0,0,0,1.35-.11A8,8,0,0,0,183.89,94.66Z"></path>
-          </svg>
-        )}
-        <h1>{title}</h1>
-        <h2>{description}</h2>
+
+
+<div className="headings">
+        <h3>{title}</h3>
+        <h4>{description}</h4>
+        </div>
         {badges && 
           <ul className="thing-badge-container">
             {badges.map((badge) => <li className="badge">{badge}</li>)}
@@ -60,19 +54,28 @@ const Things = () => (
     <ul className="mason">
     <li>
         <ThingCard
-          type="art"
+          link="https://github.com/Shaun-Anderson/folio"
+          title="folio"
+          description="The code for this site."
+          badges={[
+            "Astro", "React", "Vanilla CSS"
+          ]}
+          compact
+        />
+      </li>
+    <li>
+        <ThingCard
           link="https://figcv.com"
           title="figcv"
           description="Simple Resume/CV generator that look good."
           badges={[
-            "React", "NextJs", "Supabase", "Tailwind", "TRPC"
+            "React", "NextJS", "Supabase", "Tailwind", "TRPC"
           ]}
           wip
         />
       </li>
       <li>
       <ThingCard
-          type="art"
           link="https://twitter.com/ando_null/status/1767468325838487788"
           title="inkt"
           description="Writing companion for novelists and scriptwriters."
@@ -85,11 +88,15 @@ const Things = () => (
       </li>
       <li>
         <ThingCard
-          type="art"
           link="https://solr.fyi"
           title="Solr"
           description="Freelance management without the stress."
           wip
+          badges={[
+            "React", "NextJS", "PlanetScale", "Tailwind", "TRPC"
+          ]}
+          img={{src: solr, alt: "Screenshot of inkt app", pos: "bottom"}}
+
         />
       </li>
     </ul>
